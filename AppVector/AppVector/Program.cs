ad270e385dad5ff76629e1d5bc2688ee129eff8e
Estudiante PUCESE <estudiante@pucese.edu.ec>
2018-11-22 09:27:15 -0500

    Bloque Try Catch

--- AppVector/AppVector/Program.cs
+++ AppVector/AppVector/Program.cs
@@ -17,8 +17,37 @@ namespace AppVector
             Console.WriteLine("Control de Versiones4 ");
             Console.WriteLine("Control de Versiones5 ");
             Console.WriteLine("Probando Control de Versiones ");
+            Console.WriteLine("3 elevado a la 4 es igual a : {0}", Potencia(3, 4));
+            try
+            {
+                Console.WriteLine("-5 elevado a la 2 es igual a : {0}", Potencia(-5, 2));
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message.ToString());
+            }
+            Console.WriteLine("2 elevado a la 5 es igual a : {0}", Potencia(2, 5));
 
             Console.ReadKey();
+        }
+
+        public static double Potencia(double x, double y)
+        {
+            if(x <= 0)
+            {
+                throw new Exception(" X debe ser Positivo..");
+            }
+            if (y <= 0 )
+            {
+                throw new Exception(" Y debe ser Positivo..");
+            }
+
+            double p = 1;
+            for (int i = 1; i <= y; i++)
+            {
+                p = p * x;
+                
+            }
+            return p;
         }
     }
 }
